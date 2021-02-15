4d2344a2c0bc9642a21906fb39da33b3f1426804
Ishita Khurana <42705855+khuranaishita@users.noreply.github.com>
2021-02-14 21:59:33 -0800

    Time: 80 ms (63.82%), Space: 39 MB (29.63%) - LeetHub

---
+++ two-sum/two-sum.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  numsIndexes = {};
+
+  for (let i = 0; i < nums.length; i += 1) {
+    let currentDifference = target - nums[i];
+
+    if (numsIndexes[currentDifference] !== undefined && numsIndexes[currentDifference] !== i) {
+      return [i, numsIndexes[currentDifference]];
+    } else {
+      numsIndexes[nums[i]] = i;
+    }
+  }
+};
