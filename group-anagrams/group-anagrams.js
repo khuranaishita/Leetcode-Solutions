16164226e47322f5d2f8e53c07357378a68e3313
Ishita Khurana <42705855+khuranaishita@users.noreply.github.com>
2021-02-17 22:52:56 -0800

    Time: 148 ms (44.94%), Space: 49 MB (86.13%) - LeetHub

---
+++ group-anagrams/group-anagrams.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+    let hash = {}
+
+    strs.forEach(str => {
+        let letters = str.split('').sort()
+
+        hash[letters] ? hash[letters].push(str) : hash[letters] = [str]
+    })
+
+    const keys = Object.keys(hash);
+    const values = keys.map(function(v) { return hash[v]; });
+    return values
+
+};
