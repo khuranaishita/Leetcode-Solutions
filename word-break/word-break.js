6a17f995982c1cd869496f1adc89a421335ed209
Ishita Khurana <42705855+khuranaishita@users.noreply.github.com>
2021-03-10 22:23:37 -0800

    Time: 88 ms (63.92%), Space: 40.8 MB (43.20%) - LeetHub

---
+++ word-break/word-break.js
@@ -0,0 +1,23 @@
+/**
+ * @param {string} s
+ * @param {string[]} wordDict
+ * @return {boolean}
+ */
+const wordBreak = (s, wordDict) => {
+  if(!wordDict) return false;
+  let dp = new Array(s.length + 1);
+  dp[0] = true; //word of length 0 is always true;
+  let matches=[];
+  for(let i = 1; i <= s.length; i++) {
+    //i denotes that word length.
+    for(let j = 0; j<i; j++) {
+	  	if(dp[i]) break; //will not need to set dp[i] if it's already true
+      if(dp[j] && wordDict.indexOf(s.substring(i,j)) >= 0) {
+//dp[j] = previous substring, s.substring(i,j) = remaining substring
+        dp[i] = true;
+        break;
+      }
+    }
+  }
+  return Boolean(dp[s.length]);
+};
